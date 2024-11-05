dcbc18f01686c3bf4a69556a6b5d1e8df10c7383
sujoncoder <sujonsheikh.in@gmail.com>
2024-11-05 12:57:53 +0600

    done apiAlice

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,32 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Video from "./Video";
+import VideoLoader from "../ui/loaders/VideoLoader"
+import Error from "../ui/Error"
 
 export default function Videos() {
+    const { data: videos, isError, isLoading } = useGetVideosQuery();
+
+    let content = null;
+
+    if (isLoading) {
+        content = <VideoLoader />
+    }
+
+    if (!isLoading && isError) {
+        content = <Error message="There was an error" />
+    }
+
+    if (!isLoading && !isError && videos?.length === 0) {
+        content = <Error message="No videos found !" />
+    }
+
+    if (!isLoading && !isError && videos?.length > 0) {
+        content = videos.map((video) => <Video key={video.id} video={video} />)
+    }
+
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            {content}
         </>
     );
 }
